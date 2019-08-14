20e11bc0fcfaa6d88f0ba750ff5f26581e404d15
Tommy Coleman <tommiilee@gmail.com>
2019-08-14 15:53:00 -0400

    setup file structure, made a reducer and actions file, and attached add feature action to button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import {createStore } from 'redux'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { featuresReducer } from './reducer/featuresReducer';
+export const store = createStore(featuresReducer);
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
